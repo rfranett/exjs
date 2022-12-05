736307c59f9107ea187fb160da5b6d59fbeec69e
rfranett <rfranett@gmail.com>
2022-12-05 17:06:25 -0300

    crie uma estrutura

---
+++ exjs/ex001.js
@@ -0,0 +1,16 @@
+let lutador = 'ATON.ATN.ERR'
+let nascionalidade = 'Brasileiro'
+let idade = 2
+let peso = 5
+let altura = 80
+
+
+document.write(`<div class="card">
+                <h2>lutador</h2>
+                <img src="blankos14.jpg">
+                <p>${lutador} </p>
+                <p>${nascionalidade} </p>
+                <p>${idade} anos  </p>
+                <p>${peso} kg </p>
+                <p>${altura} cm </p>
+                </div>`)
