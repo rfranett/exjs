4992bf8a97ee96e02baed7973990b2b0377dd525
rfranett <rfranett@gmail.com>
2022-12-06 14:38:35 -0300

    colocando mais um lutador

--- exjs/ex001.js
+++ exjs/ex001.js
@@ -1,8 +1,15 @@
 let lutador = 'ATON.ATN.ERR'
 let nascionalidade = 'Brasileiro'
-let idade = 2
+let idade = 1
 let peso = 5
 let altura = 80
+
+let lutador1 = 'POng'
+let nascionalidade1 = 'Brasileiro'
+let idade1 = 1
+let peso1 = 5
+let altura1 = 80
+
 
 
 document.write(`<div class="card">
@@ -14,3 +21,16 @@ document.write(`<div class="card">
                 <p>${peso} kg </p>
                 <p>${altura} cm </p>
                 </div>`)
+
+                document.write(`<div class="card">
+                <h2>lutador</h2>
+                <img src="blankos17.jpg">
+                <p>${lutador1} </p>
+                <p>${nascionalidade1} </p>
+                <p>${idade1} anos  </p>
+                <p>${peso1} kg </p>
+                <p>${altura1} cm </p>
+                </div>`)
+
+
+
